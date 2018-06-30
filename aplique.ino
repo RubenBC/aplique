e2b9de231dba6a2c09bce0734128387810866805
ch3ca <rbcheca@gmail.com>
2018-06-30 04:57:15 +0200

    añadido contador de tiempo pulsado del boton

--- aplique.ino
+++ aplique.ino
@@ -1,13 +1,42 @@
-void setup()
-{
+int led = 2;
+int pulsador = 4;
 
-  /* add setup code here */
 
+
+//PULSACION
+boolean estado_pulsador;
+boolean corta;
+boolean larga;
+
+//temporizacion para pulsadores
+
+unsigned long inicio_pulsacion = 0;
+unsigned long t_pulsado = 0;
+
+
+
+void setup() {
+	pinMode(led, OUTPUT);
+	pinMode(pulsador, INPUT);
+
+
+	Serial.begin(115200);
 }
 
-void loop()
-{
+void loop() {
+	//leo el estado de las entradas
+	estado_pulsador = digitalRead(pulsador);
 
-  /* add main program code here */
+	if (estado_pulsador == HIGH && inicio_pulsacion == 0) {
+		inicio_pulsacion = millis();//inicializo el tiempo e impido que vuelva a funcionara hasta T_actual valga cero,
 
+
+	}
+	//calculo de la duraccion de la pulsación (T_pulsado)inicio_pulsacion
+	if (estado_pulsador == LOW && inicio_pulsacion != 0) {
+
+		t_pulsado = millis() - inicio_pulsacion;
+		inicio_pulsacion = 0;
+		Serial.println(t_pulsado/10);
+	}
 }
